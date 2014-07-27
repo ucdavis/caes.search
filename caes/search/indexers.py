a3951804d2ed1cc64b8159172e92d3ddf9ddebe3
Elizabeth Leddy <eleddy@umich.edu>
2014-07-27 10:59:17 -0700

    holy moly, failing local error that was never supposed to fail. doh

--- caes/search/indexers.py
+++ caes/search/indexers.py
@@ -8,18 +8,19 @@ import re
 def splash_keywords(object):
     try:
         final = []
-        # handle poorly entered keywords
-        for keyword in object.keywords:
-            multiple = re.split('[,.:;]', keyword)
-            if len(multiple) > 1:
-                final += multiple
-                continue
-            final.append(keyword)
+        if object.keywords:
+            # handle poorly entered keywords
+            for keyword in object.keywords:
+                multiple = re.split('[,.:;]', keyword)
+                if len(multiple) > 1:
+                    final += multiple
+                    continue
+                final.append(keyword)
         return final
     except AttributeError, e:
-        logging.errror("Tried to index an object with ISplashU interface,'+ \
+        logging.error("Tried to index an object with ISplashU interface,'+ \
                       ' but no keywords item - %s: %s" % (object.absolute_url(),
                                                           e))
     except:  # should never fail save because of this
-        logging.error("Could not index for splash inclusion - %s: %s" % (object.absolute_url(),
-                                                                         e))
+        import pdb;pdb.set_trace()
+        logging.error("Could not index for splash inclusion - %s: %s" % (object.absolute_url()))
